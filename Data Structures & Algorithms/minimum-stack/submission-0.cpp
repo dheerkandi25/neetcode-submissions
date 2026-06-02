1e30c4284587d6517c461733416d383de7f83af9
Dheeraj Kandikattu <kandikattu.dheeraj@gmail.com>
2026-06-02 18:21:04 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,35 @@
+class MinStack {
+public:
+    stack<int>stk;
+    stack<int>minStk;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stk.push(val);
+
+        if (minStk.empty() || val <= minStk.top()) {
+            minStk.push(val);
+        }
+        
+    }
+    
+    void pop() {
+        int val = stk.top();
+
+        if (val == minStk.top()) {
+            minStk.pop();
+        }
+        stk.pop();
+        
+    }
+    
+    int top() {
+       return stk.top();
+    }
+    
+    int getMin() {
+        return minStk.top();
+    }
+};
