58f1821d2354de83d0b842a5b8a267356cbde7b3
Dheeraj Kandikattu <kandikattu.dheeraj@gmail.com>
2026-07-29 18:01:43 -0400

    Add: interleaving-string - submission-2

---
+++ Data Structures & Algorithms/interleaving-string/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int dfs(int i,int j,string s1,string s2,string s3){
+        if(i==s1.length() && j==s2.length()) {
+            return 1;
+        }
+        if(dp[i][j]!=-1) {
+            return dp[i][j];
+        }
+        if(i<s1.length() && s1[i]==s3[i+j] && dfs(i+1,j,s1,s2,s3)==1) {
+            return 1;
+        }
+        if(j<s2.length() && s2[j]==s3[i+j] && dfs(i,j+1,s1,s2,s3)==1) {
+            return 1;
+        }
+        return dp[i][j]=0;
+        
+        
+    }
+     bool isInterleave(string s1, string s2, string s3) {
+        dp.resize(s1.length()+1,vector<int>(s2.length()+1,-1));
+        if(s1.length()+s2.length()!=s3.length()) {
+            return false;
+        }
+        if(dfs(0,0,s1,s2,s3)==1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+        
+        
+    }
+};
